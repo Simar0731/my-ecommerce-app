5917e9d9293b7faf8d00611efdf42ef1fb276fa8
simar <ajargaming847@gmail.com>
2024-07-09 19:18:13 +0530

    src->styles-> GlobalStyles.js

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Arial', sans-serif;
+    background-color: #f5f5f5;
+    color: #333;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export default GlobalStyles;
